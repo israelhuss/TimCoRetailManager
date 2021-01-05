620c97358180cb574a39ed69d27f86925a8194ee
Israel Huss <israelmhuss@gmail.com>
2021-01-05 11:44:42 -0500

    123

---
+++ TRMDataManager/App_Start/AuthTokenOperation.cs
@@ -0,0 +1,14 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Web.Http.Description;
+
+namespace TRMDataManager.App_Start
+{
+	public class AuthTokenOperation : IDocumentFilter
+	{
+		public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}

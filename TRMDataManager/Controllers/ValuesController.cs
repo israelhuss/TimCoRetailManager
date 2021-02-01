bcb16b90422c93d83119a09630e03718f83fd656
Israel Huss <israelmhuss@gmail.com>
2021-02-01 14:25:33 -0500

    Updated Packages

--- TRMDataManager/Controllers/ValuesController.cs
+++ TRMDataManager/Controllers/ValuesController.cs
@@ -1,40 +1,38 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace TRMDataManager.Controllers
 {
-    [Authorize]
-    public class ValuesController : ApiController
-    {
-        // GET api/values
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+	[Authorize]
+	public class ValuesController : ApiController
+	{
+		// GET api/values
+		public IEnumerable<string> Get()
+		{
+			string userId = RequestContext.Principal.Identity.GetUserId();
+			return new string[] { "value1", "value2", userId };
+		}
 
-        // GET api/values/5
-        public string Get(int id)
-        {
-            return "value";
-        }
+		// GET api/values/5
+		public string Get(int id)
+		{
+			return "value";
+		}
 
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
+		// POST api/values
+		public void Post([FromBody] string value)
+		{
+		}
 
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+		// PUT api/values/5
+		public void Put(int id, [FromBody] string value)
+		{
+		}
 
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
-    }
+		// DELETE api/values/5
+		public void Delete(int id)
+		{
+		}
+	}
 }

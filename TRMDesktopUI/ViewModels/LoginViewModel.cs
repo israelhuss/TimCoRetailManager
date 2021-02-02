2853b298567613676ef72dae1aaf60afe97697cb
Israel Huss <israelmhuss@gmail.com>
2021-02-02 01:36:41 -0500

    Created the Login Form

---
+++ TRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Caliburn.Micro;
+
+namespace TRMDesktopUI.ViewModels
+{
+	public class LoginViewModel : Screen
+	{
+		private string _userName;
+		private string _password;
+
+		public string UserName
+		{
+			get { return _userName; }
+			set
+			{
+				_userName = value;
+				NotifyOfPropertyChange(() => UserName);
+				NotifyOfPropertyChange(() => CanLogIn);
+			}
+		}
+
+		public string Password
+		{
+			get { return _password; }
+			set
+			{
+				_password = value;
+				NotifyOfPropertyChange(() => Password);
+				NotifyOfPropertyChange(() => CanLogIn);
+			}
+		}
+
+		public bool CanLogIn => UserName?.Length > 0 && Password?.Length > 5;
+
+		public void LogIn(string userName, string password)
+		{
+		}
+	}
+}
--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -1,6 +1,16 @@
-﻿namespace TRMDesktopUI.ViewModels
+﻿using Caliburn.Micro;
+
+namespace TRMDesktopUI.ViewModels
 {
-	public class ShellViewModel
+	public class ShellViewModel : Conductor<object>
 	{
+
+		private LoginViewModel _loginVM;
+
+		public ShellViewModel(LoginViewModel loginVM)
+		{
+			_loginVM = loginVM;
+			ActivateItem(_loginVM);
+		}
 	}
 }

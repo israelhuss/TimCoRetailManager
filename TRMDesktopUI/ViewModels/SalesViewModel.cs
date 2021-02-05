a7318837e25f945a1b216beb455996422af7a9ef
Israel Huss <israelmhuss@gmail.com>
2021-02-05 14:28:53 -0500

    AddToCart is working

--- TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Caliburn.Micro;
 using System.ComponentModel;
+using System.Linq;
 using System.Threading.Tasks;
 using TRMDesktopUI.Library.Api;
 using TRMDesktopUI.Library.Models;
@@ -9,14 +10,15 @@ namespace TRMDesktopUI.ViewModels
 	public class SalesViewModel : Screen
 	{
 		private BindingList<ProductModel> _products;
-		private int _itemQuantity;
-		private BindingList<string> _cart;
+		private ProductModel _selectedProduct;
+		private int _itemQuantity = 1;
+		private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
 		private IProductEndpoint _productEndpoint;
 
 		public SalesViewModel(IProductEndpoint productEndpoint)
 		{
 			_productEndpoint = productEndpoint;
-			
+
 		}
 
 		protected override async void OnViewLoaded(object view)
@@ -41,6 +43,17 @@ namespace TRMDesktopUI.ViewModels
 			}
 		}
 
+		public ProductModel SelectedProduct
+		{
+			get { return _selectedProduct; }
+			set
+			{
+				_selectedProduct = value;
+				NotifyOfPropertyChange(() => SelectedProduct);
+				NotifyOfPropertyChange(() => CanAddToCart);
+			}
+		}
+
 		public int ItemQuantity
 		{
 			get { return _itemQuantity; }
@@ -48,11 +61,11 @@ namespace TRMDesktopUI.ViewModels
 			{
 				_itemQuantity = value;
 				NotifyOfPropertyChange(() => ItemQuantity);
+				NotifyOfPropertyChange(() => CanAddToCart);
 			}
 		}
 
-
-		public BindingList<string> Cart
+		public BindingList<CartItemModel> Cart
 		{
 			get { return _cart; }
 			set
@@ -62,25 +75,57 @@ namespace TRMDesktopUI.ViewModels
 			}
 		}
 
-		public string SubTotal => "$0.00";
+		public string SubTotal
+		{
+			get
+			{
+				decimal subTotal = 0;
+
+				foreach (var item in Cart)
+				{
+					subTotal += (item.Product.RetailPrice * item.QuantityInCart);
+				}
+				return subTotal.ToString("C");
+			}
+		}
 		public string Tax => "$0.00";
 		public string Total => "$0.00";
 
 
-
 		// TODO - Make sure something is selected...
-		public bool CanAddToCart => false;
+		public bool CanAddToCart => ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity;
 		public bool CanRemoveFromCart => false;
 		public bool CanCheckOut => false;
 
 		public void AddToCart()
 		{
+			CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
 
+			if (existingItem != null)
+			{
+				existingItem.QuantityInCart += ItemQuantity;
+				Cart.Remove(existingItem);
+				Cart.Add(existingItem);
+			}
+			else
+			{
+				CartItemModel item = new CartItemModel
+				{
+					Product = SelectedProduct,
+					QuantityInCart = ItemQuantity
+				};
+				Cart.Add(item);
+			}
+
+			SelectedProduct.QuantityInStock -= ItemQuantity;
+			ItemQuantity = 1;
+			NotifyOfPropertyChange(() => SubTotal);
 		}
 
 		public void RemoveFromCart()
 		{
 
+			NotifyOfPropertyChange(() => SubTotal);
 		}
 
 		public void CheckOut()

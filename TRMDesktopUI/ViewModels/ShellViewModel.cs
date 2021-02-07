2d8d1cf245b6056567bb07285c6d42725c654e3a
Israel Huss <israelmhuss@gmail.com>
2021-02-07 13:27:42 -0500

    Replace container ref in ShellviewModel

--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using Caliburn.Micro;
-using System;
 using TRMDesktopUI.EventModels;
 
 namespace TRMDesktopUI.ViewModels
@@ -8,17 +7,15 @@ namespace TRMDesktopUI.ViewModels
 	{
 		private IEventAggregator _events;
 		private SalesViewModel _salesVM;
-		private SimpleContainer _container;
 
-		public ShellViewModel(IEventAggregator events, SalesViewModel salesVM, SimpleContainer container)
+		public ShellViewModel(IEventAggregator events, SalesViewModel salesVM)
 		{
 			_events = events;
 			_salesVM = salesVM;
-			_container = container;
 
 			_events.Subscribe(this);
 
-			ActivateItem(_container.GetInstance<LoginViewModel>());
+			ActivateItem(IoC.Get<LoginViewModel>());
 		}
 
 		public void Handle(LogOnEvent message)

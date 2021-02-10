74599b019df6a012894c1e25f5e97899de47747f
israelmhuss <israelmhuss@gmail.com>
2021-02-10 15:12:46 -0500

    Fix Logoff bug

--- TRMDesktopUI.Library/Api/APIHelper.cs
+++ TRMDesktopUI.Library/Api/APIHelper.cs
@@ -58,6 +58,11 @@ namespace TRMDesktopUI.Library.Api
 			}
 		}
 
+		public void LogOffUser()
+		{
+			_apiClient.DefaultRequestHeaders.Clear();
+		}
+
 		public async Task GetLoggedInUserInfo(string token)
 		{
 			_apiClient.DefaultRequestHeaders.Clear();

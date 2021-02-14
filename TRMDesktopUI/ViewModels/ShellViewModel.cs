a68a8c87471b77d4b9e1abbe56725c4032aceea0
Israel Huss <israelmhuss@gmail.com>
2021-02-14 16:12:25 -0500

    Add user endpoint and user page

--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -31,6 +31,11 @@ namespace TRMDesktopUI.ViewModels
 			TryClose();
 		}
 
+		public void UserManagement()
+		{
+			ActivateItem(IoC.Get<UserDisplayViewModel>());
+		}
+
 		public void LogOut()
 		{
 			_user.ResetUserModel();

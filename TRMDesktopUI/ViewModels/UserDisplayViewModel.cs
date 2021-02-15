7d0e345bf3dbe60d0f1104dd4e827c541bea7c86
israelmhuss <israelmhuss@gmail.com>
2021-02-15 14:03:54 -0500

    remove warning for not awaited call

--- TRMDesktopUI/ViewModels/UserDisplayViewModel.cs
+++ TRMDesktopUI/ViewModels/UserDisplayViewModel.cs
@@ -40,7 +40,11 @@ namespace TRMDesktopUI.ViewModels
 			{
 				_selectedUser = value;
 				UserRoles = new BindingList<string>(value.Roles.Select(x => x.Value).ToList());
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 				LoadRoles();
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
 				NotifyOfPropertyChange(() => SelectedUser);
 			}
 		}
